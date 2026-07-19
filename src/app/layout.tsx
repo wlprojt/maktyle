ce3abc3d074bb7dac4e8efc1b5470fde780609f3
Rakesh Vishwas <rickyvishwas11@gmail.com>
2026-07-19 19:47:52 +0530

    toplod

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Navbar />
+        <NextTopLoader color="#8348de" />
         {children}
         <Footer />
         </body>
